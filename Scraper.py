c6171338367676cefaf6b4385441ee0320979c5a
pkapusta <piotrkap97@gmail.com>
2023-10-17 00:15:23 +0200

    corrected load

--- Scraper.py
+++ Scraper.py
@@ -1,55 +1,74 @@
 import pandas as pd
 import yfinance as yf
+import credentials
+
+import queue
 
 import logging
 
+from sqlalchemy.types import String, Float, Date, BigInteger
 from sqlalchemy import create_engine
 
 from datetime import timedelta
 
+user = credentials.user
+password = credentials.password
+db = credentials.db
+host = credentials.host
+port = credentials.port
 
+engine = create_engine('postgresql://{0}:{1}@{2}:{3}/{4}'.format(user, 
+                                                                  password,
+                                                                  host,
+                                                                  port,
+                                                                  db))
 
+types = {'Symbol':String(10),
+         'Date':Date,
+         'Open':Float,
+         'High':Float,
+         'Low':Float,
+         'Close':Float,
+         'Adj Close':Float,
+         'Volume':BigInteger()
+         }
 
 #First load
-'''
-engine = create_engine('postgresql://postgres:postgres@localhost:5432/postgres')
+'''            
 ticker_list = ['AAPL', 'TSLA', 'CDR.WA', 'MSFT', '^GSCP', 'GC=F', 'CL=F']
 start_date = '2000-1-1'
 
-for symbol in ticker_list:
-    if symbol == 'AAPL':
-        df = yf.download(symbol, start=start_date)
-        df.to_sql(symbol, engine, schema='Trading', if_exists='replace')
-    elif symbol == 'TSLA':
-        df = yf.download(symbol, start=start_date)
-        df.to_sql(symbol, engine, schema='Trading', if_exists='replace')
-    elif symbol == 'CDR.WA':
-        df = yf.download(symbol, start=start_date)
-        df.to_sql(symbol, engine, schema='Trading', if_exists='replace')
-    elif symbol == 'MSFT':
-        df = yf.download(symbol, start=start_date)
-        df.to_sql(symbol, engine, schema='Trading', if_exists='replace')
-    elif symbol == '^GSCP':
-        df = yf.download(symbol, start=start_date)
-        df.to_sql(symbol, engine, schema='Trading', if_exists='replace')
-    elif symbol == 'GC=F':
-        df = yf.download(symbol, start=start_date)
-        df.to_sql(symbol, engine, schema='Trading', if_exists='replace')
-    elif symbol == 'CL=F':
-        df = yf.download(symbol, start=start_date)
-        df.to_sql(symbol, engine, schema='Trading', if_exists='replace')
-'''
+columns = ['Symbol', 'Date', 'Open', 'High', 'Low', 'Close', 'Adj Close', 'Volume']
+df = pd.DataFrame(columns=columns)
 
-def get_historical_data(symbol, start_date, table_name):
+
+
+for symbol in ticker_list:
+    tmp = yf.download(symbol, start=start_date)
+    tmp['Symbol'] = symbol
+    tmp = tmp.reset_index()
+    df = pd.concat([df, tmp])        
+    df.to_sql('Stocks', engine, schema='Trading', if_exists='replace', index=False,
+              dtype=types)
+
+'''
+    
+def get_historical_data(symbol, start_date):
     engine = create_engine('postgresql://postgres:postgres@localhost:5432/postgres')
     values = yf.download(symbol, start_date)
-    values.to_sql(table_name, engine, schema='Trading', if_exists='replace')
+    values.to_sql('Stocks', engine, schema='Trading', if_exists='append', index=False,
+                  dtype=types)
     
 def get_recent_data(symbol):
     engine = create_engine('postgresql://postgres:postgres@localhost:5432/postgres')
     query = 'select max("Date") from {0}.{1}'.format('"Trading"', '"' + symbol + '"')
     start_date = pd.read_sql(query, engine)[max][0] + timedelta(days=1)
-    values = yf.download(symbol, start=start_date )
-    values.to_sql(symbol, engine, schema="Trading", if_exists='append')
+    df = yf.download(symbol, start=start_date )
+    df = preprocess(df, symbol)
+    df.to_sql('Stocks', engine, schema="Trading", if_exists='append', index=False,
+              dtype=types)
 
+def preprocess(df, symbol):
+    df['SYMBOL'] = symbol
+    return df
 
