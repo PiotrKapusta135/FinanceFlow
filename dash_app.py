3d23f076934262cbe6b47a74f1421f7450de57ea
pkapusta <piotrkap97@gmail.com>
2023-11-15 23:51:50 +0100

    playing with dash

--- dash_app.py
+++ dash_app.py
@@ -1,7 +1,9 @@
-from dash import Dash, dcc, html
+from dash import Dash, dcc, html, dash_table
 import pandas as pd
 import config_file
 from sqlalchemy import create_engine
+from dash.dependencies import Input
+from dash.dependencies import Output
 
 
 user = config_file.user
@@ -17,20 +19,35 @@ url = 'postgresql://{0}:{1}@{2}:{3}/{4}'.format(user,
                                                 db)
 
 engine = create_engine(url)
-query = 'select * from "Trading"."Stocks" where "Symbol" = \'AAPL\''
+query = 'select * from "Trading"."Stocks" where "Symbol" in (\'AAPL\', \'TSLA\')'
 
 data = pd.read_sql(query, engine)
+symbols = data['Symbol'].unique().tolist()
 
 data.head()
 
+
 app = Dash(__name__)
 
+'''app.layout = html.Div([
+    html.Div(children='My first dash app'),
+    dash_table.DataTable(data=data.to_dict('records'), page_size=10)
+    ])
+
+app.run()'''
+
 app.layout = html.Div(
-    children=[
-        html.H1(children='Stocks Analysis'),
-        html.P(children=('App for stock analysis - to be improved'),
-        ),
+    html.Div([
+        html.H1('Stocks Analysis'),
+        html.P('App for stock analysis - to be improved'),
+        dcc.Dropdown(
+            id='datatable_dropdown',
+            options=[{"label":sy, "value":sy} for sy in symbols],
+            placeholder='Select symbol',
+            multi=False,
+            value=data['Symbol'].values,),
         dcc.Graph(
+            id='line_graph',
             figure={
                 'data':[
                     {
@@ -42,6 +59,24 @@ app.layout = html.Div(
                 'layout':{'title':'Open price of Apple Stocks'},
                 },
             ),
+        dash_table.DataTable(
+            id='datatable',
+            columns=[{'name':i, "id":i} for i in data.columns],
+            data=data.to_dict('records'), page_size=10)]))
+
+@app.callback(
+    Output('datatable', 'data'),
+    Input('datatable_dropdown', 'value'))
+
+@app.callback(
+    Output('line_graph', 'data'),
+    Input('datatable_dropdown', 'value'))
+
+def display_table(symbol):
+    symbol_x = data[data.Symbol.isin([symbol])]
+    return symbol_x.to_dict("records")  
+        
+'''       
         dcc.Graph(
             figure={
                 'data':[
@@ -56,5 +91,7 @@ app.layout = html.Div(
             ),
         ]
     )
-
-app.run_server(debug=True)
+    ]
+)'''
+    
+app.run(debug=True)
