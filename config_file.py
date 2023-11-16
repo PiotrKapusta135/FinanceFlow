329131e9a7ffd021c8224d05b58a0cb13ab76a68
pkapusta <piotrkap97@gmail.com>
2023-11-16 23:47:59 +0100

    adjusted slicers

--- config_file.py
+++ config_file.py
@@ -5,3 +5,7 @@ port = 5432
 db = 'postgres'
 
 symbols = ['AAPL', 'TSLA', 'CDR.WA', 'MSFT', '^GSPC', 'GC=F', 'CL=F']
+
+usernames_passwords = {
+    'admin':'admin'
+    }
--- dash_app.py
+++ dash_app.py
@@ -1,9 +1,13 @@
 from dash import Dash, dcc, html, dash_table
+from dash.dependencies import Input
+from dash.dependencies import Output
+import dash_auth
+
+import plotly.express as px
+
 import pandas as pd
 import config_file
 from sqlalchemy import create_engine
-from dash.dependencies import Input
-from dash.dependencies import Output
 
 
 user = config_file.user
@@ -23,40 +27,36 @@ query = 'select * from "Trading"."Stocks" where "Symbol" in (\'AAPL\', \'TSLA\')
 
 data = pd.read_sql(query, engine)
 symbols = data['Symbol'].unique().tolist()
+kpis = ['Open', 'High', 'Low', 'Close', 'Adj Close',
+       'Volume']
 
 data.head()
 
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
-app = Dash(__name__)
-
-'''app.layout = html.Div([
-    html.Div(children='My first dash app'),
-    dash_table.DataTable(data=data.to_dict('records'), page_size=10)
-    ])
-
-app.run()'''
+app = Dash(__name__, external_stylesheets=external_stylesheets)
+#auth = dash_auth.BasicAuth(app, config_file.usernames_passwords)
 
 app.layout = html.Div(
     html.Div([
-        html.H1('Stocks Analysis'),
-        html.P('App for stock analysis - to be improved'),
+        html.H1(children='Stocks Analysis', style={'textAlign':'center'}),
+        html.P(children='App for stock analysis - to be improved',  style={'textAlign':'center'}),
+        html.Hr(),
         dcc.Dropdown(
-            id='datatable_dropdown',
+            id='symbol_dropdown',
             options=[{"label":sy, "value":sy} for sy in symbols],
-            placeholder='Select symbol',
+            placeholder='Select stock',
             multi=False,
-            value=data['Symbol'].values,),
+            value='AAPL'),
+        dcc.Dropdown(
+            id='kpi_dropdown',
+            options=[{'label':kpi, 'value':kpi} for kpi in kpis],
+            multi=False,
+            value='Open',
+            placeholder='Select KPI'),
         dcc.Graph(
             id='line_graph',
             figure={
-                'data':[
-                    {
-                        'x':data['Date'],
-                        'y':data['Open'],
-                        'type':'lines'
-                        },
-                    ],
-                'layout':{'title':'Open price of Apple Stocks'},
                 },
             ),
         dash_table.DataTable(
@@ -65,33 +65,22 @@ app.layout = html.Div(
             data=data.to_dict('records'), page_size=10)]))
 
 @app.callback(
-    Output('datatable', 'data'),
-    Input('datatable_dropdown', 'value'))
+    Output('line_graph', 'figure'),
+    [Input('symbol_dropdown', 'value'), Input('kpi_dropdown', 'value')])
+def upgrade_graph(symbol, kpi):
+    fig = px.line(data.loc[data['Symbol']==symbol],
+                  'Date', 
+                  kpi,
+                  title='{0} price of {1} Stocks'.format(kpi, symbol),)
+    return fig
 
 @app.callback(
-    Output('line_graph', 'data'),
-    Input('datatable_dropdown', 'value'))
-
+    Output('datatable', 'data'),
+    Input('symbol_dropdown', 'value'))
 def display_table(symbol):
     symbol_x = data[data.Symbol.isin([symbol])]
     return symbol_x.to_dict("records")  
         
-'''       
-        dcc.Graph(
-            figure={
-                'data':[
-                    {
-                        'x':data['Date'],
-                        'y':data['Close'],
-                        'type':'lines'
-                        },
-                    ],
-                'layout':{'Close price of Apple Stocks'},
-            },
-            ),
-        ]
-    )
-    ]
-)'''
+
     
 app.run(debug=True)
